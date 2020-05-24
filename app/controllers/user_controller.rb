69d9936e9fd6117e8b2d1794ff948aa1f249a8ad
Dylan O'Carroll <dylancocarroll@gmail.com>
2020-05-24 10:51:46 -0700

    auth after signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,6 @@ class UserController < ApplicationController
   def create
     req_body = JSON.parse(request.body.read)
     u = User.create(req_body)
-    render json: u
+    render json: u.auth_payload
   end
 end
